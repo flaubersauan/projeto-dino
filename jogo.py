bc9602e30df6bc355a6feb33ce5f50548bbe8511
Sauan <flabersauan3@gmail.com>
2025-02-22 17:45:35 -0300

    adicionando obstaculos e colisões entre os objetos

--- jogo.py
+++ jogo.py
@@ -2,7 +2,7 @@ import pygame
 from sys import exit
 from pygame.locals import *
 import os
-from random import randrange
+from random import randrange, choice
 
 pygame.init()
 
@@ -24,6 +24,13 @@ tela = pygame.display.set_mode((LARGURA,ALTURA))
 pygame.display.set_caption('Dino')
 
 sprite_sheet = pygame.image.load(os.path.join(imagens, 'dinoSpritesheet.png')).convert_alpha()
+som_colisao = pygame.mixer.Sound(os.path.join(sons, 'death_sound.wav'))
+som_colisao.set_volume(1)
+
+colidiu = False
+
+escolha_obstaculo = choice([0,1])
+
 
 class Dino(pygame.sprite.Sprite):
     def __init__(self):
@@ -39,6 +46,7 @@ class Dino(pygame.sprite.Sprite):
         self.atual = 0
         self.image = self.img_dinossauro[self.atual]
         self.rect =  self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
         self.rect.center = (100,ALTURA-60)
         self.pulo = False
         self.pos_inicial_y = ALTURA - 60 - 96//2 #Pegando o centro da imagem pois o rect pega a parte superior esquerda
@@ -76,7 +84,7 @@ class Nuvem(pygame.sprite.Sprite):
             self.rect.x = LARGURA
             self.rect.y = randrange(50,200,50)
             self.x = LARGURA - randrange(30,300,90)
-        self.rect.x -=7
+        self.rect.x -=15
 
 class Chao(pygame.sprite.Sprite):
     def __init__(self, pos_x):
@@ -90,7 +98,49 @@ class Chao(pygame.sprite.Sprite):
     def update(self):
         if self.rect.topright[0] < 0:
             self.rect.x = LARGURA
-        self.rect.x -=7
+        self.rect.x -=15
+
+class Cacto(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = sprite_sheet.subsurface((5*32, 0, 32,32))
+        self.image = pygame.transform.scale(self.image, (32*2,32*2))
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.escolha = escolha_obstaculo
+        self.rect.center = (LARGURA, ALTURA-60)
+        self.rect.x = LARGURA
+        
+    def update(self):
+        if self.escolha == 0:
+            if self.rect.topright[0] < 0:
+                self.rect.x = LARGURA
+            self.rect.x -=15
+class DinoVoador(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.img_dinossauro = []
+        for i in range(3,5):
+            img = sprite_sheet.subsurface((i * 32, 0, 32,32))
+            img = pygame.transform.scale(img,(32*3, 32*3))
+            self.img_dinossauro.append(img)
+        self.atual = 0
+        self.image = self.img_dinossauro[self.atual]
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect.center = (LARGURA, 300)
+        self.rect.x = LARGURA
+        self.escolha = escolha_obstaculo
+
+    def update(self):
+        if self.escolha == 1:
+            if self.rect.topright[0] < 0:
+                self.rect.x = LARGURA
+            self.rect.x -=15
+            if self.atual >=1:
+                self.atual = 0
+            self.atual += 0.25
+            self.image = self.img_dinossauro[int(self.atual)]
 
 todas_sprites = pygame.sprite.Group()
 
@@ -105,6 +155,22 @@ for i in range(4):
 for i in range(LARGURA*2//64):#Lagura da tela (640px) dividido por cada frame do objeto chão (64px).Obs; Fiz uma gambiarra pra que o chão não quebrasse
     chao = Chao(i)
     todas_sprites.add(chao)
+
+cacto = Cacto()
+
+todas_sprites.add(cacto)
+
+grupo_obstaculos = pygame.sprite.Group()
+
+grupo_obstaculos.add(cacto)
+
+dino_voador = DinoVoador()
+
+todas_sprites.add(dino_voador)
+
+grupo_obstaculos.add(dino_voador)
+
+
 relogio = pygame.time.Clock()
 
 while True:
@@ -120,7 +186,25 @@ while True:
                     pass
                 else:
                     dino.pular()
-    todas_sprites.draw(tela)
-    todas_sprites.update()
+        
+    colisoes = pygame.sprite.spritecollide(dino,grupo_obstaculos, False, pygame.sprite.collide_mask) #Lista que recebe os obstáculos que o dino colide
+    todas_sprites.draw(tela)#Desenha na tela todas as sprites.
+
+    if cacto.rect.topright[0] <= 0 or dino_voador.rect.topright[0] <= 0:
+        escolha_obstaculo = choice([0,1])
+        cacto.rect.x = LARGURA
+        dino_voador.rect.x = LARGURA
+        cacto.escolha = escolha_obstaculo
+        dino_voador.escolha = escolha_obstaculo
+    if colisoes and colidiu == False:
+        som_colisao.play()
+        colidiu = True
+
+    if colidiu == True:
+        pass
+
+    else:
+        todas_sprites.draw(tela)
+        todas_sprites.update()#Atualiza o movimento das sprites.
     
     pygame.display.flip()
